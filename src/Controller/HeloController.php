bbd7a7bf258313eb9c58b4433b02ff3149e82141
s14003 <s14003@std.it-college.ac.jp>
2016-08-08 11:43:48 +0900

    Section4 end

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -1,31 +1,16 @@
 <?php
 namespace App\Controller;
- 
+
 use App\Controller\AppController;
- 
-class HeloController extends AppController {
-    public function index($a = '', $b = '123') {
 
-       
-	if ($a == '') {
-	    $this->setAction('err');
-	    return; 
+class HeloController extends AppController
+{
+	public function index() {
+		$str = $this->request->data('text1');
+		$msg = 'typed:' . $str;
+		if ($str == null) {
+			$msg = "please type...";
+		}
+		$this->set('message', $msg);
 	}
-
-	$this->autoRender = false;
-        echo "<html><head></head><body>";
-        echo "<h1>Hello!</h1>";
-        echo "<p>これは、サンプルで作成したページです.</p>";
-        echo "</body></html>";
-	echo "</p></body></html>";
-	
-	    	
-    }
-    public function err() {
-	$this->autoRender = false;
-        echo "<html><head></head><body>";
-        echo "<h1>Hello!</h1>";
-        echo "<p>パラメータがありませんでした。</p>";
-        echo "</body></html>";
-    }
 }
